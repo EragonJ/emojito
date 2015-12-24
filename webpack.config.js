7fb2a689eeca6cde92f37cc6831c99544a050ad2
EragonJ <eragonj@eragonj.me>
2015-12-25 00:21:12 +0800

    add some base files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/emojito.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'emojito.js'
+    library: 'Emojito',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
